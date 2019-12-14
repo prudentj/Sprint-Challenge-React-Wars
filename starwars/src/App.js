c16f28437d32d8ec5442fdf1ea32ac42a293f585
Jacob Prudent <jake.prudent@gmail.com>
2019-12-13 23:10:54 -0800

    Fixed the syntax error

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './App.css';
-
+import axios from 'axios'
+import SpacePerson from './components/SpacePerson'
+import styled from 'styled-components'
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -8,10 +10,38 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const[spacePeople, setSpacePeople] = useState([])
+  const cardStack =styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content:center;
+    width:100%;
+    background: black;
+  `
+  useEffect(()=>{
+    axios.get("https://lambda-swapi.herokuapp.com/api/people/")
+      .then(res => {
+        console.log(res.data.results)
+        setSpacePeople(res.data.results)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },[])
 
+    
+  var peopleHolder = [];
+  
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <cardStack>
+        {spacePeople.map((i)=>{return(
+        <SpacePerson name={i.name} birth_year={i.birth_year}/> )})
+        }
+      </cardStack>
+      
+      
     </div>
   );
 }
